56230f45abe0ea81194e8d4f25c0791d0b051332
Jeyabal <jeyabal1993@gmail.com>
2022-06-19 17:59:55 +0530

    adding in data and js

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-fetch("data.json")
-.then(function(response){
-    return response.json();
-})
-.then(function(info){
-    let placeholder = document.querySelector("#title");
-    let out = '${info.title}';
-    
-    placeholder.innerHTML = out;
-})
+// fetch data from data.json and iterate through the data.body and display the sectionDetails in div the html page
+
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'data.json', true);
+xhr.onload = function () {
+    if (this.status == 200) {
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        var output = "";
+        var i;
+        var data2 = data.body;
+        for (i = 0; i < data2.length; i++) {
+            output += "<ul>";
+            output += data2[i].sectionName;
+            output += "<li>" + data2[i].sectionDetails + "</li>";
+            output += "</ul>";
+
+        }
+        document.getElementById("data").innerHTML = output;
+    }
+}
+
+xhr.send();
